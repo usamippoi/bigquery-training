483ccf69708b445201bb5e31f52c7515c7c9445e
usamippoi <usamippoi@gmail.com>
2022-09-22 02:18:41 +0900

    list型の中身の型を変換するクエリを作成

--- repeat/create-table.sql
+++ repeat/create-table.sql
@@ -4,3 +4,10 @@ SET @@dataset_id = 'MyDataset';
 BEGIN
   CREATE OR REPLACE TABLE repeattest (id STRING, name STRING, reptest ARRAY<STRING>);
 END;
+
+create table project_id.detaset_id.output as
+with tmp as (
+select id, name, array_agg(cast(list as struct<numeric>)) as list 
+from project_id.detaset_id.import, unnest(list_in_list.list) as list group by id, name
+)
+select id, name, list from tmp;
